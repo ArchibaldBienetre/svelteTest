27468ebe140215d67e3dbfd616f288a282f34d17
hoobert <bluewhobert@mailbox.org>
2022-11-12 20:48:54 +0100

    run VSCode formatter

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,13 @@
 <script>
-    import FileChooser from './lib/components/FileChooser.svelte'
+  import FileChooser from "./lib/components/FileChooser.svelte";
 </script>
 
 <main>
   <h1>Svelte example app</h1>
 
   <div class="card">
-    <FileChooser/>
+    <FileChooser />
   </div>
-
 </main>
 
 <style>
--- src/lib/components/FileChooser.svelte
+++ src/lib/components/FileChooser.svelte
@@ -1,38 +1,36 @@
-
 <!-- mostly copied + adjusted from example here: https://svelte.dev/tutorial/select-bindings -->
 <script>
+	// for now, let's just hard-code the options and all
 
-    // for now, let's just hard-code the options and all
+	const image1Text = `Welsh Corgy`;
+	const image2Text = `Beagle`;
+	const image3Text = `Bernese Mountain Dog`;
+	const toolTipTextSuffix = ` - isn't it a cute one?`;
 
-    const image1Text = `Welsh Corgy`;
-    const image2Text = `Beagle`;
-    const image3Text = `Bernese Mountain Dog`;
-    const toolTipTextSuffix = ` - isn't it a cute one?`;
-
-    import image1 from '../../assets/dogImage1.jpg'
-    import image2 from '../../assets/dogImage2.jpg'
-    import image3 from '../../assets/dogImage3.jpg'
+	import image1 from "../../assets/dogImage1.jpg";
+	import image2 from "../../assets/dogImage2.jpg";
+	import image3 from "../../assets/dogImage3.jpg";
 
 	const imageOptions = [
 		{ id: "image1", text: image1Text },
 		{ id: "image2", text: image2Text },
-		{ id: "image3", text: image3Text }
+		{ id: "image3", text: image3Text },
 	];
 
-    const chosenImageIdToImageData = {
-        "image1": {
-            image: image1,
-            text: image1Text
-        },
-        "image2": {
-            image: image2,
-            text: image2Text
-        },
-        "image3": {
-            image: image3,
-            text: image3Text
-        }
-    };
+	const chosenImageIdToImageData = {
+		image1: {
+			image: image1,
+			text: image1Text,
+		},
+		image2: {
+			image: image2,
+			text: image2Text,
+		},
+		image3: {
+			image: image3,
+			text: image3Text,
+		},
+	};
 
 	let selected;
 	let imageToDisplay = image1;
@@ -45,14 +43,16 @@
 		imageToDisplay = imageData.image;
 		imageToDisplayAltText = imageData.text;
 		imageToDisplayToolTip = imageData.text + toolTipTextSuffix;
-		console.debug(`The selected option is ${imageToDisplay? "truthy" : "falsy"}`);
+		console.debug(
+			`The selected option is ${imageToDisplay ? "truthy" : "falsy"}`
+		);
 	}
 </script>
 
 <h2>Choose a picture</h2>
 
 <form on:submit|preventDefault={handleSubmit}>
-	<select id="imageSelect" bind:value={selected} >
+	<select id="imageSelect" bind:value={selected}>
 		{#each imageOptions as imageOption}
 			<option value={imageOption}>
 				{imageOption.text}
@@ -60,13 +60,16 @@
 		{/each}
 	</select>
 
-	<button id="sumbitSelection" type=submit>
-		Submit
-	</button>
+	<button id="sumbitSelection" type="submit"> Submit </button>
 </form>
 
 <!-- Alternatively, could try start with an empty selection and CSS attribute "display: none;" or something -->
-<img id="imageDisplay" alt="{imageToDisplayAltText}" title="{imageToDisplayToolTip}" src="{imageToDisplay}" />
+<img
+	id="imageDisplay"
+	alt={imageToDisplayAltText}
+	title={imageToDisplayToolTip}
+	src={imageToDisplay}
+/>
 
 <style>
 	input {
@@ -75,6 +78,6 @@
 		max-width: 100%;
 	}
 	img {
-	    height: 500px;
+		height: 500px;
 	}
 </style>
