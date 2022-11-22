1737f0864064fb63f23dc6ff9c8bf2afc899733f
hoobert <bluewhobert@mailbox.org>
2022-11-22 22:15:06 +0100

    implement zigzagging properly

--- src/lib/transitions/custom-transitions.js
+++ src/lib/transitions/custom-transitions.js
@@ -4,23 +4,45 @@ const NUMBER_OF_WAYPOINTS = 10;
 const SCALE_OF_ZIG_ZAG_MOTION = 100;
 
 /**
+ * @param easedPosition = value between 0 and 1 (but should tolerate other values)
  * @param numberOfWaypoints = number of points where the zigzagging changes direction
  **/
 export function getZigZagXPosition(easedPosition, numberOfWaypoints) {
 
-	const scaledProgress = easedPosition * numberOfWaypoints;
+	// "* numberOfWayPoints * 2X", so that we will have an even number and the movement stops at "0" position
+	const scaleFactor = numberOfWaypoints * 2;
+	const scaledProgress = easedPosition * scaleFactor;
 
 	const roundedScaledProgress = Math.floor(scaledProgress);
-	const legGoesUp = roundedScaledProgress % 2 === 0;
+	// add a full interval to get a positive remainder
+	const remainder = (roundedScaledProgress + numberOfWaypoints * scaleFactor) % 4;
+	const legGoesUpPositive = remainder === 0;
+	const legGoesDownPositive = remainder === 1;
+	const legGoesDownNegative = remainder === 2;
+	const legGoesUpNegative = remainder === 3;
 
 	const positionOnCurrentLeg = scaledProgress - roundedScaledProgress;
 
-	if (legGoesUp) {
-		return positionOnCurrentLeg;
-	} else {
-		return -1 * positionOnCurrentLeg;
-	}
+	console.debug(`Remainder: ${remainder}`);
+	console.debug(`Scaled progress: ${scaledProgress}`);
+	console.debug(`Rounded scaled progress: ${roundedScaledProgress}`);
+	console.debug(`Position on current leg: ${positionOnCurrentLeg}`);
 
+	let v;
+	if (legGoesUpPositive) {
+		console.debug("leg goes up positive")
+		return positionOnCurrentLeg;
+	} else if (legGoesDownPositive) {
+		console.debug("leg goes down positive")
+		return (1 - positionOnCurrentLeg);
+	} else if (legGoesDownNegative) {
+		console.debug("leg goes down negative")
+		return (-1 * positionOnCurrentLeg);
+	} else if (legGoesUpNegative) {
+		console.debug("leg goes up negative")
+		return (-1 + positionOnCurrentLeg);
+	}
+	throw "error calculating zigzag transition";
 }
 
 export function zigzag(node, { duration }) {
